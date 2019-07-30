f671c918decb58d9733b0bced4f7b7e3a52f714e
Ulrik Danielsen <ulrik_danielsen@hotmail.com>
2019-07-30 10:44:16 +0200

    last

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
-DRBUG = True
+DEBUG = True
 
 
 ALLOWED_HOSTS = [
