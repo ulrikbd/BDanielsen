4c105a0d64664d33c2f3660d9715606991057bfb
Ulrik Danielsen <ulrik_danielsen@hotmail.com>
2019-11-15 21:27:36 +0100

    linux!

---
+++ bdanielsen_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ulrikbd/Documents/web_applications/BDanielsen/bdanielsen_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
